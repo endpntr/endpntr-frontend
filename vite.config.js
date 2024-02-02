46d81b2955ffd75a1533b5705e85835fd05d65f9
Brandon Corey <bcorey3660@gmail.com>
2024-02-02 10:44:32 -0500

    more build for production

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {},
 
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:3000",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 });
