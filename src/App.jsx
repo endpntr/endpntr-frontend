4983b739b8b7d00e0083f8dc6251e5c6e6384fed
Brandon Corey <bcorey3660@gmail.com>
2024-02-10 15:27:54 -0500

    moves router to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Payload from "./component/endpoint/payload";
 import PayloadPlaceholder from "./component/endpoint/payload-placeholder";
 import Endpoint from "./component/endpoint/endpoint";
@@ -6,19 +6,17 @@ import LandingPage from "./component/landing/landing-page";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route
-          path="/:endpointHash"
-          element={<Endpoint PayloadType={PayloadPlaceholder} />}
-        />
-        <Route
-          path="/:endpointHash/:requestHash"
-          element={<Endpoint PayloadType={Payload} />}
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route
+        path="/:endpointHash"
+        element={<Endpoint PayloadType={PayloadPlaceholder} />}
+      />
+      <Route
+        path="/:endpointHash/:requestHash"
+        element={<Endpoint PayloadType={Payload} />}
+      />
+    </Routes>
   );
 }
 
