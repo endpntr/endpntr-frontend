01eac769ec29b4c029b9a53ef53755bcc55f9a6c
Brandon Corey <bcorey3660@gmail.com>
2024-02-11 00:23:29 -0500

    centers landing page

--- src/component/landing/landing-page.jsx
+++ src/component/landing/landing-page.jsx
@@ -6,7 +6,7 @@ function LandingPage() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
       <Header />
-      <main className="flex flex-1 justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <main className="flex flex-1 items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div>
           <div className="max-w-md w-full space-y-8 text-center">
             <h2 className="mt-6 text-3xl font-extrabold text-gray-900 dark:text-gray-100">
