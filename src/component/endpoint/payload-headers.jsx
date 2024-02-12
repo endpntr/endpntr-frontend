37caa6bf4942b2959ecfe977e917a8a540ccc8f7
Brandon Corey <bcorey3660@gmail.com>
2024-02-11 20:03:59 -0500

    allows you to collapse and expand headers

--- src/component/endpoint/payload-headers.jsx
+++ src/component/endpoint/payload-headers.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 function PayloadHeaders({ headers }) {
+  const headersPreview = headers.slice(0, 3);
+  const [currHeaders, setHeaders] = useState(headersPreview);
+
+  const toggleHeaders = () => {
+    const result = currHeaders === headers ? headersPreview : headers;
+    setHeaders(() => result);
+  };
+
   return (
     <>
       <div className="flex-wrap bg-gray-700 border border-gray-500 p-2 mb-4 shadow-xl">
-        <dl className="w-1/2">
-          {headers.map((header) => {
+        <dl className="w-1/2 my-1">
+          {currHeaders.map((header) => {
             return (
               <fieldset
                 className="flex flex-1 gap-10 border-b border-gray-500 py-2"
@@ -15,6 +25,12 @@ function PayloadHeaders({ headers }) {
             );
           })}
         </dl>
+        <button
+          className="focus:outline-none p-1 rounded-md text-gray-500 transition ease-in delay-50 hover:text-white"
+          onClick={toggleHeaders}
+        >
+          {headers === currHeaders ? "Collapse" : "Expand"}
+        </button>
       </div>
     </>
   );
